3c7961a1e04ec202517fde493c603863b9a44b8d
Rupal Jain <rupaljain.1699@gmail.com>
2020-09-19 20:55:05 +0530

    created a list called guesses to store all the guessed numbers

--- script.js
+++ script.js
@@ -4,14 +4,15 @@
  * DONE: Generate a random number 1 to 100 and save it to variable correctNumber
  * DONE: Console whether the guess is too high, too low, or is correct inside playGame function
  * DONE: Create a function called displayResult to move the logic for if the guess is too high, too low, or correct
- * TODO: Complete the showYouWon, showNumberAbove, showNumberBelow
- * TODO: Use the showYouWon... functions within displayResult to display the correct dialog
- * TODO: Save the guess history in a variable called guess
+ * DONE: Complete the showYouWon, showNumberAbove, showNumberBelow
+ * DONE: Use the showYouWon... functions within displayResult to display the correct dialog
+ * DONE: Save the guess history in a variable called guess
  * TODO: Display the guess history using displayHistory() function
  * TODO: Use the initGame() function to restart the game
  */
 
 // Variable to store the list of guesses
+let guesses = [];
 
 // Variable for store the correct random number
 
@@ -30,6 +31,7 @@ function playGame() {
   // *CODE GOES BELOW HERE *
   let numberGuess = document.getElementById("number-guess").value;
   console.log(displayResult(numberGuess));
+  saveGuessHistory(numberGuess);
 }
 
 /**
@@ -82,6 +84,9 @@ function getRandomNumber() {
  */
 function saveGuessHistory(guess) {
   // *CODE GOES BELOW HERE *
+  guesses.push(guess);
+  console.log(guesses);
+  return guesses;
 }
 
 /**
